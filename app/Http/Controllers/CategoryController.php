672aecb36f821a636d3f698d8ac5783ccf75de8c
Sergio Rios <sergioriosp04@gmail.com>
2020-02-11 17:30:14 -0500

    se agregro el metodo para guardar categorias

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,6 +9,11 @@ use App\Category;
 class CategoryController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('api.auth', ['except'=>['index', 'show']]);
+    }
+
     public function index(){
         $categories = Category::all();
         return \response()->json([
@@ -31,6 +36,44 @@ class CategoryController extends Controller
                 'code' => 404,
                 'status'=> 'error',
                 'message' => 'la categorias con id:'. $id . ' no existe'
+            ];
+        }
+        return response()->json($data, $data['code']);
+    }
+
+    public function store(Request $request){
+        //recoger datos por post
+        $json = $request->input('json', null);
+        $params_array = json_decode($json, true);
+
+        if($params_array){
+            // validar datos
+            $validate = \Validator::make($params_array, [
+                'name' => 'required',
+            ]);
+
+            // guardar category
+            if($validate->fails()){
+                $data = [
+                    'code' => 400,
+                    'status' => 'error',
+                    'message' => $validate->errors()
+                ];
+            }else{
+                $category = new Category();
+                $category->name = $params_array['name'];
+                $category->save();
+                $data = [
+                    'code' => 200,
+                    'status' => 'success',
+                    'category' => $category
+                ];
+            }
+        }else{
+            $data = [
+                'code' => 400,
+                'status' => 'error',
+                'message' => 'No se ha enviado ningun dato'
             ];
         }
         return response()->json($data, $data['code']);
